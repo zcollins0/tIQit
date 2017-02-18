c9113ca78e26a774d892bd1a16e3dd7fe577b145
Zach Collins <collinzy@mail.uc.edu>
2017-02-18 00:46:42 -0500

    oh no i worked on master

--- controllers/ticket.js
+++ controllers/ticket.js
@@ -1,13 +1,20 @@
 var tickets = require('../models/ticket');
 
 exports.view_all = function(req, res) {
-
+    tickets.find(function(err, tickets) {
+        res.render('/ticket/view-all', {
+            title: "All Tickets",
+            tickets: tickets
+        });
+    });
 }
 
 exports.view = function(req, res) {
     tickets.findOne({"ticketSchema._id": req.params.id}, function(err, ticket) {
         if (ticket) {
-            
+            res.render('/ticket/view', {
+                title: ticket.problemTitle
+            });
         }
     });
 }
