d32aa58b6f38ec1fbab671670670bda21b42d349
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-02-18 00:46:20 -0500

    Create initial post request

--- controllers/ticket.js
+++ controllers/ticket.js
@@ -17,5 +17,17 @@ exports.create = function(req, res) {
 }
 
 exports.submit = function(req, res) {
+    var ticket = Ticket();
+    ticket.hackerName = req.body.hackerName;
+    ticket.hackerPhoneNumber = req.body.hackerPhoneNumber;
+    ticket.hackerLocation = req.body.hackerLocation;
+    ticket.problemTitle = req.body.problemTitle;
+    ticket.problemDesrcription = req.body.problemDescription;
 
+    company.save(function(err) {
+        if (err) {
+            res.send(err);
+        }
+    });
+    res.send(201);
 }
